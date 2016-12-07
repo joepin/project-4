e85b84755c02b5c9b672c9baa027e7eb9fcc0a82
Joey Pinhas <jpinhas97@yahoo.com>
2016-12-07 06:34:32 -0500

    Creates middlewre to handle media stream requests, and gets the server up and running

--- server/server.js
+++ server/server.js
@@ -6,8 +6,10 @@ const logger     = require('morgan');
 const app = express();
 const port = 3000;
 
+const audioStream = require('./lib/audio-stream.js');
+
 app.use(logger('dev'));
 
-app.get('/', (req, res) => res.json('Welcome to server/server.js!'));
+app.get('/', audioStream.checkFile, audioStream.getMimeType, audioStream.startStream);
 
 app.listen(port, () => console.warn(`Server listening on port 3000!`));
