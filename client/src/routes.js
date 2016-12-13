d12468b9f4b1c295b583c9d6f88c7ff2f2a099c2
Joey Pinhas <jpinhas97@yahoo.com>
2016-12-12 19:11:50 -0500

    Rearranges react src to use react router

---
+++ client/src/routes.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Route, IndexRoute, Link } from 'react-router';
+import App from './components/App.jsx';
+
+module.exports = (
+  <Route path='/' component={App}>
+  </Route>
+);
