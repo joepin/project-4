23822c5d16a0e87745f15cb964446f04ac49d36c
Joey Pinhas <jpinhas97@yahoo.com>
2016-12-14 21:07:28 -0500

    Brushes up previous commit, and clears localStorage when the token expires

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -12,11 +12,23 @@ class App extends Component {
   }
 
   componentWillMount() {
+    // get the token and token timestamp from localStorage
     const token = localStorage.getItem('userAuthToken');
     const startTime = localStorage.getItem('tokenReceived');
-    const userData = JSON.parse(localStorage.getItem('userData'));
-    this.updateState('userData', userData);
-    browserHistory.push((token && (Date.now() - startTime <= 14400000)) ? '/profile' : '/login');
+    // check that the token exists and that it was generated within the last 4 hours (which is the
+    // expiry time of the token, as defined by the server in the JWT.sign method - see ../../lib/auth.js)
+    if (token && (Date.now() - startTime <= 14400000)) {
+      // if we're logged in, then we should have user data in localStorage, so get it and store it in state
+      const userData = JSON.parse(localStorage.getItem('userData'));
+      this.updateState('userData', userData);
+      // redirect the user to their profile
+      browserHistory.push('/profile');
+    } else {
+      // if not, then clear all of localStorage just in case
+      localStorage.clear();
+      // redirect the user to the login page
+      browserHistory.push('/login');
+    }
   }
 
   updateState(key, value) {
